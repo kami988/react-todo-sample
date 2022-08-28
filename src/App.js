97878e15077e5e875f387b36423b64de5167a476
kami988 <baske.yoshi007@gmail.com>
2022-08-29 00:43:05 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import { useState, useRef } from "react";
-import TodoList from "./TodoList";
-import { v4 as uuidv4 } from "uuid";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const todoNameRef = useRef();
-
-  const handleAddTodo = () => {
-    const name = todoNameRef.current.value;
-    if (name === "") return;
-    setTodos((prevTodos) => {
-      return [...prevTodos, { id: uuidv4(), name: name, completed: false }];
-    });
-    todoNameRef.current.value = null;
-  };
-
-  const toggleTodo = (id) => {
-    const newTodos = [...todos];
-    const todo = newTodos.find((todo) => todo.id === id);
-    todo.completed = !todo.completed;
-    setTodos(newTodos);
-  };
-
-  const handleClear = () => {
-    const newTodos = todos.filter((todo) => !todo.completed);
-    setTodos(newTodos);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
